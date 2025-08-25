cd19788c0793be64d6df315c39ab1b04d53f871b
Fabian Andrees <fabian.andrees@echolon.de>
2025-08-25 16:53:34 +0200

    added helper class for administrations

---
+++ Eco.Echolon.ApiClient/Client/GraphQl/Administration/BaseAdministration.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Eco.Echolon.ApiClient.Model;
+using Eco.Echolon.ApiClient.Model.DomainTypes;
+using Eco.Echolon.ApiClient.Query;
+
+namespace Eco.Echolon.ApiClient.Client.GraphQl.Administration
+{
+    public class BaseAdministration<TItem, TItemInput> : IAdministration<TItem, TItemInput> where TItem : class
+    {
+        private readonly string _moduleName;
+        private readonly IBaseClient _graphClient;
+
+        public BaseAdministration(string moduleName, IBaseClient graphClient)
+        {
+            _moduleName = moduleName;
+            _graphClient = graphClient;
+        }
+        
+        public Task<GraphQlResponse<TItem[]>> All()
+        {
+            return _graphClient.QueryCustom<TItem[]>(GetPath(nameof(All)));
+        }
+
+        public Task<GraphQlResponse<TItem>> One(ItemId<TItem> id)
+        {
+            return _graphClient.QueryCustom<TItem>(GetPath(nameof(One)),
+                new Dictionary<string, object?>() { ["id"] = id });
+        }
+
+        public Task<GraphQlResponse<TItem>> Default()
+        {
+            return _graphClient.QueryCustom<TItem>(GetPath(nameof(Default)));
+        }
+
+        public Task<GraphQlResponse<TItem>> Store(TItemInput item)
+        {
+            return _graphClient.QueryCustom<TItem>(GetPath(nameof(Store)),
+                new Dictionary<string, object?>() { [nameof(item)] = item }, true);
+        }
+
+        // public Task<GraphQlResponse> Delete(ItemId<TItem> id)
+        // {
+        //     return _graphClient.QueryCustom<void>(GetPath(nameof(Delete)),
+        //         new Dictionary<string, object?>() { [nameof(id)] = id }, true);
+        // }
+        public Task<GraphQlResponse<TItem>> Activate(ItemId<TItem> id)
+        {
+            return _graphClient.QueryCustom<TItem>(GetPath(nameof(Activate)),new Dictionary<string, object?>(){[nameof(id)] = id}, true);
+        }
+
+        public Task<GraphQlResponse<TItem>> Deactivate(ItemId<TItem> id)
+        {
+            return _graphClient.QueryCustom<TItem>(GetPath(nameof(Deactivate)),new Dictionary<string, object?>(){[nameof(id)] = id}, true);
+        }
+        
+        private string[] GetPath(string endpoint)
+        {
+            return new[] { "admin", _moduleName, endpoint.Uncapitalize() };
+        }
+    }
+}
--- Eco.Echolon.ApiClient/Client/IApiClient.cs
+++ Eco.Echolon.ApiClient/Client/IApiClient.cs
@@ -1,10 +1,12 @@
 ﻿using Eco.Echolon.ApiClient.Client.GraphQl;
+using Eco.Echolon.ApiClient.Client.GraphQl.Administration;
 using Eco.Echolon.ApiClient.Client.RestApi;
 
 namespace Eco.Echolon.ApiClient.Client
 {
     public interface IApiClient
     {
+        public IBaseClient BaseClient { get; }
         public IWorkingClient Working { get; }
         public IFormattedTextClient FormattedText { get; }
         public IFileClient File { get; }
@@ -13,5 +15,10 @@ namespace Eco.Echolon.ApiClient.Client
         public ISystemClient System { get; }
         public IViewClient Views { get; }
         public IWorkingQueueClient WorkingQueue { get; }
+
+        public IVersionedAdministrationFor<TItem, TItemInput, TListItem>
+            VersionedAdministration<TItem, TItemInput, TListItem>(string module) where TItem : class;
+        
+        public IAdministration<TItem, TItemInput> Administration<TItem, TItemInput>(string module) where TItem : class;
     }
 }
