3b3a60a2c032a55efc338f3179bb59988a80024a
Fabian Andrees <fabian.andrees@echolon.de>
2025-08-25 16:53:13 +0200

    fixed small issues in query and serialization

---
+++ Eco.Echolon.ApiClient/Query/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Eco.Echolon.ApiClient.Query
+{
+    public static class StringExtensions
+    {
+        public static string Uncapitalize(this string str)
+        {
+            return string.Concat(str[0].ToString().ToLower(),
+                str.Substring(1, str.Length - 1));
+        }
+    }
+}
