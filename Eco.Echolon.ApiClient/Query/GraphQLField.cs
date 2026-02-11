11902bc2b1b1a7e86db89bcfc5543997d0d23125
Fabian Andrees <fabian.andrees@echolon.de>
2026-02-11 15:33:02 +0100

    own class

--- Eco.Echolon.ApiClient/Query/GraphQLField.cs
+++ Eco.Echolon.ApiClient/Query/GraphQLField.cs
@@ -72,39 +72,4 @@ namespace Eco.Echolon.ApiClient.Query
             return new GraphQLField(s, null);
         }
     }
-
-    public class InterfaceGraphQLField : GraphQLField
-    {
-        public Dictionary<string, GraphQLField[]> Variants { get; } = new Dictionary<string, GraphQLField[]>();
-
-        public InterfaceGraphQLField(string name,
-            GraphQLField[]? baseChildren = null,
-            IDictionary<string, object?>? arguments = null)
-            : base(name, baseChildren, arguments)
-        {
-        }
-
-        public void AddVariant(string name, GraphQLField[] conditionalChildren)
-        {
-            Variants.Add(name, conditionalChildren);
-        }
-
-        public override StringBuilder ToString(StringBuilder builder)
-        {
-            return ToString(builder,
-                x =>
-                {
-                    foreach (var kv in Variants)
-                    {
-                        x.Append($"... on {kv.Key} {{");
-                        foreach (var graphQLField in kv.Value)
-                        {
-                            graphQLField.ToString(x);
-                        }
-
-                        x.Append("}");
-                    }
-                });
-        }
-    }
 }
